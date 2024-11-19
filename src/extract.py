1d689f93105f241dca8b31878becf7c5fb630a9e
Nicolas Perez Caro <nicolas.perez.caro@outlook.com>
2024-11-18 21:17:06 -0500

    fix(get_df_from_csv): handle missing files

--- src/extract.py
+++ src/extract.py
@@ -1,5 +1,8 @@
+import os
 import polars as pl
 
 
 def get_df_from_csv(file_path: str, separator: str = ";") -> pl.DataFrame:
+    if not os.path.exists(path=file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
     return pl.read_csv(source=file_path, separator=separator)

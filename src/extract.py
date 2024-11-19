002977b1a93b781d03a1172d84761f61d8ba1e37
Nicolas Perez Caro <nicolas.perez.caro@outlook.com>
2024-11-18 21:22:20 -0500

    chore(get_df_from_csv): add docstrings

--- src/extract.py
+++ src/extract.py
@@ -3,6 +3,19 @@ import polars as pl
 
 
 def get_df_from_csv(file_path: str, separator: str = ";") -> pl.DataFrame:
+    """
+    This function takes a path to a CSV file and returns a Polars DataFrame.
+
+    Args:
+        file_path: The path to the CSV file intended to be used.
+        separator: The delimeter used in the CSV file. Defaults to ";".
+
+    Returns:
+        pl.DataFrame: A DataFrame that contains the CSV data.
+
+    Raises:
+        FileNotFoundError: if the provided path leads to a file that does not exist.
+    """
     if not os.path.exists(path=file_path):
         raise FileNotFoundError(f"File not found: {file_path}")
     return pl.read_csv(source=file_path, separator=separator)

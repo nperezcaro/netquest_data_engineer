7bc83d6850fd86051bc985733de775d5610cd157
Nicolas Perez Caro <nicolas.perez.caro@outlook.com>
2024-11-18 21:03:09 -0500

    test: add test for handling missing file error

--- tests/test_df_builder.py
+++ tests/test_df_builder.py
@@ -13,3 +13,7 @@ def test_load_valid_csv(valid_csv):
     df = get_df_from_csv(valid_csv)
     assert isinstance(df, pl.DataFrame)
     assert df.shape == (5, 6)
+
+
+def test_file_not_found():
+    assert get_df_from_csv(file_path="non_existing_file.csv") == FileNotFoundError

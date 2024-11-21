3180ba62e61c4e1d4f404db6e8de44d8f656dbbb
Nicolas Perez Caro <nicolas.perez.caro@outlook.com>
2024-11-21 07:54:04 -0500

    test(handle_dimensions): add test to handle missing columns

--- tests/test_handle_duplicate_dimensions.py
+++ tests/test_handle_duplicate_dimensions.py
@@ -58,3 +58,34 @@ def test_empty_df():
 
     with pytest.raises(ValueError):
         handle_dimensions(df=df)
+
+
+def test_missing_columns():
+    input_df = pl.DataFrame(
+        {
+            "id": [1, 1, 1, 2, 3],
+            "Channel": ["channel1", "channel1", "channel2", "channel3", "channel2"],
+            "Language": ["en", "en_us", "en", "es", "es"],
+            "CustomFields": [
+                "Area=account;New=true",
+                "Area=account;New=true",
+                "Area=finance;New=false",
+                "Area=finance;Premium=premium-user;New=false",
+                "Area=customer;New=false",
+            ],
+            "Duration": ["1:23:14", "0:13:04", "0:37:21", "3:01:47", "1:56:34"],
+            "PointsGained": [57, 12, 30, 254, 71],
+            "ChannelB": ["Channel1", "Channel1", "Channel2", "Channel3", "Channel2"],
+            # Missing "LanguageB" column,
+            "CustomFieldsB": [
+                "area=Accounting;New=true",
+                "area=Accounting;New=true",
+                "area=Finance;New=false",
+                "area=Finance;premium=VIP_User;New=false",
+                "area=Customer_Care;New=false",
+            ],
+        }
+    )
+
+    with pytest.raises(ValueError):
+        handle_dimensions(df=input_df)

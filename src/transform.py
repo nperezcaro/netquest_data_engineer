0bcecce9e66562416997de39d2668e6469f84f52
Nicolas Perez Caro <nicolas.perez.caro@outlook.com>
2024-11-21 07:42:12 -0500

    feat(handle_dimensions): add function that handle dimensions duplicates

--- src/transform.py
+++ src/transform.py
@@ -78,3 +78,56 @@ def map_custom_fields(df: pl.DataFrame, mapping_dict: Dict[str, str]) -> pl.Data
         .list.join(";")  # Join all back into a str
         .alias("CustomFieldsB")
     )
+
+
+def handle_dimensions(df: pl.DataFrame) -> pl.DataFrame:
+    duration_split_df = df.with_columns(
+        # '^(\d+):' captures the digits before the first colon (hours)
+        pl.col("Duration").str.extract(r"^(\d+):", 1).cast(pl.Int64).alias("hours"),
+        # ':(\d+):' captures the digits between two colons (minutes)
+        pl.col("Duration").str.extract(r":(\d+):", 1).cast(pl.Int64).alias("minutes"),
+        # ':(\d+)$' captures the digits after the last colon (seconds)
+        pl.col("Duration").str.extract(r":(\d+)$", 1).cast(pl.Int64).alias("seconds"),
+    )
+
+    total_seconds_df = duration_split_df.with_columns(
+        (pl.col("hours") * 3600 + pl.col("minutes") * 60 + pl.col("seconds")).alias(
+            "TotalSeconds"
+        )
+    )
+
+    unique_dimensions_df = total_seconds_df.group_by(
+        ["id", "ChannelB", "LanguageB", "CustomFieldsB"], maintain_order=True
+    ).agg(
+        [
+            pl.sum("PointsGained").alias("PointsGained"),
+            pl.sum("TotalSeconds").alias("TotalSeconds"),
+        ]
+    )
+
+    dimensions_duration_df = unique_dimensions_df.with_columns(
+        (
+            # zfill(2) ensures that each component has two digits ("03" instead of "3")
+            (pl.col("TotalSeconds") // 3600).cast(pl.Int64).cast(pl.Utf8).str.zfill(2)
+            + ":"
+            + ((pl.col("TotalSeconds") % 3600) // 60)
+            .cast(pl.Int64)
+            .cast(pl.Utf8)
+            .str.zfill(2)
+            + ":"
+            + (pl.col("TotalSeconds") % 60).cast(pl.Int64).cast(pl.Utf8).str.zfill(2)
+        ).alias("Duration")
+    )
+
+    result_df = dimensions_duration_df.select(
+        ["id", "ChannelB", "LanguageB", "CustomFieldsB", "Duration", "PointsGained"]
+    )
+
+    # Replace to the original column names
+    return result_df.rename(
+        mapping={
+            "ChannelB": "Channel",
+            "LanguageB": "Language",
+            "CustomFieldsB": "CustomFields",
+        }
+    )
